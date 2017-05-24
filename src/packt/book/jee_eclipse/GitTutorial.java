6ae7eebbff258dfbf2f914f33d8cee182d6764b7
kashifjt <kashifjt@gmail.com>
2017-05-24 19:07:28 +0200

    Added hello message.

--- src/packt/book/jee_eclipse/GitTutorial.java
+++ src/packt/book/jee_eclipse/GitTutorial.java
@@ -3,7 +3,7 @@ package packt.book.jee_eclipse;
 public class GitTutorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
 
 	}
 

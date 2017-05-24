d4693cc4873c321d6c52496e39104f0275e98484
kashifjt <kashifjt@gmail.com>
2017-05-24 19:17:07 +0200

    Modifed hello message to fix bug#1234.

--- src/packt/book/jee_eclipse/GitTutorial.java
+++ src/packt/book/jee_eclipse/GitTutorial.java
@@ -3,7 +3,7 @@ package packt.book.jee_eclipse;
 public class GitTutorial {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git, from branch bug#1234!!!");
 
 	}
 
